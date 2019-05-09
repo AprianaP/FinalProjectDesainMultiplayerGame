96b02aaa204f56b2eebc86bb080dc6c998fc9087
Apriana <aprianapramadita@gmail.com>
2019-05-09 11:58:52 +0700

    Initial commit

---
+++ Title/Assets/SampleClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MainClass
+{
+    public static void Main()
+    {
+        IPEndPoint ip = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8001);
+
+        Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            server.Connect(ip);
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("Unable to connect to server.");
+            return;
+        }
+
+        
+        
+
+        server.Shutdown(SocketShutdown.Both);
+        server.Close();
+    }
+}
