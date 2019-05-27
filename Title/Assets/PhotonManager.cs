f7021ebc48b627cc7b5e8a40c414d7d98228233d
AprianaP <36907256+AprianaP@users.noreply.github.com>
2019-05-27 20:28:48 +0700

    Add files via upload

---
+++ Title/Assets/PhotonManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonManager : Photon.MonoBehaviour {
+
+    public GameObject player;
+    [SerializeField]
+    private GameObject LobbyCamera;
+
+    // Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("1.0");
+	}
+	
+	void OnJoinedLobby () {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions() { maxPlayers = 2 }, TypedLobby.Default);
+	}
+
+    void OnJoinedRoom() {
+        PhotonNetwork.Instantiate("mc1", player.transform.position, Quaternion.identity, 0);
+        LobbyCamera.SetActive(false);
+    }
+}
