1045c158788c5acbfbd1e3a7c531c873f4337ede
AprianaP <36907256+AprianaP@users.noreply.github.com>
2019-05-30 07:57:50 +0700

    Add files via upload

---
+++ Title/Assets/PlayerNetwork.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNetwork : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject PlayerCam;
+    [SerializeField]
+    private MonoBehaviour[] ScriptToIgnore;
+
+    PhotonView photonview;
+    
+    // Use this for initialization
+	void Start () {
+        photonview = GetComponent<PhotonView>();
+        
+		
+	}
+
+    void initialize() {
+        if (photonview.isMine)
+        {
+        }
+        else
+        {
+            PlayerCam.SetActive(false);
+            foreach (MonoBehaviour item in ScriptToIgnore)
+            {
+                //item.gameObject.SetActive(false);
+                item.enabled = false;
+            }
+        }
+    }
+}
