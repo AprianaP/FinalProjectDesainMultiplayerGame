eec97a92cc3df73cc4b70b6de9fa4d6553badaab
AprianaP <36907256+AprianaP@users.noreply.github.com>
2019-06-12 23:42:02 +0700

    Update PlayerMovement.cs

--- Title/Assets/PlayerMovement.cs
+++ Title/Assets/PlayerMovement.cs
@@ -1,23 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerMovement : MonoBehaviour {
     
     Rigidbody2D rb;
 
+    float dirX;
+
+    [SerializeField]
+    float movespeed = 5f, jumpforce = 500f;
+
+    bool facingright = true;
+
+    Vector3 localscale;
+    public Rigidbody2D bullet;
+
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
+
+        localscale = transform.localScale;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKey(KeyCode.LeftArrow))
-            transform.Translate(-3 * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Translate(3 * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.UpArrow))
-            rb.AddForce(Vector2.up * 4, ForceMode2D.Impulse);
-	}
+
+    void Update()
+    {
+        dirX = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+            Jump();
+
+        //if (CrossPlatformInputManager.GetButtonDown("Fire"))
+            //Fire();
+    }
+
+    void FixedUpdate()
+    { }
+
+    void CheckFacingWhere()
+    {
+        if (dirX > 0)
+            facingright = true;
+        else if (dirX < 0)
+            facingright = false;
+        if (((facingright) && (localscale.x < 0)) || ((facingright) && (localscale.x > 0)))
+            localscale.x *= -1;
+        transform.localScale = localscale;
+    }
+    
+    void Jump()
+    {
+        if (rb.velocity.y == 0)
+            rb.AddForce(Vector2.up * jumpforce);
+    }
 }
